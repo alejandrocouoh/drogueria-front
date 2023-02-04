6cb6e8559f747901f5ba62c36478fc29e19428ad
Alejandro Couoh Haas <alejandro.couoh@gmail.com>
2023-02-03 22:58:44 -0600

    Se corrigieron los estilos en el archivo angular.json, se hicieron correcciones en el componente de proctos para listar los productos y en el template, se agrefaron los metodos para agregar un nuevo producto, modificar un producto y eliminar un producto

--- src/app/componente/producto/producto.component.ts
+++ src/app/componente/producto/producto.component.ts
@@ -1,5 +1,6 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 import { Productos } from 'src/app/model/Productos';
 import { ProductoService } from 'src/app/servicio/producto.service';
 
@@ -13,6 +14,8 @@ export class ProductoComponent implements OnInit{
 
   productos:Productos[] = [];
   columnas:any[] = [];
+  items:MenuItem[] = [];
+  displayitems:boolean = false;
 
   constructor(private productosService:ProductoService){
 
@@ -21,14 +24,10 @@ export class ProductoComponent implements OnInit{
   obtenerProductos(){
     this.productosService.listarProductos().subscribe(
       (result:any)=>{
-        let productos:Productos[] = [];
         for(let i = 0; i<result.length; i++){
           let producto = result[i] as Productos;
           this.productos.push(producto);
         }
-        this.productos = productos;
-      }, error=>{
-        console.log(error)
       }
     );
   }
@@ -42,7 +41,7 @@ export class ProductoComponent implements OnInit{
       {field: "elaboracion", header: "Elaboracion"},
       {field: "caducidad", header: "Caducidad"},
       {field: "stock", header: "Stock"},
-      {field: "precio", header: "Precio"},
+      {field: "precio", header: "Precio"}
     ];
   }
 
